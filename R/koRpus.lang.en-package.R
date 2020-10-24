02398c23dbaf3f0f62984702216c92a6f438129f
unDocUMeantIt <meik.michalke@hhu.de>
2020-10-24 17:37:42 +0200

    released as 0.1-4

--- R/koRpus.lang.en-package.R
+++ R/koRpus.lang.en-package.R
@@ -5,7 +5,7 @@
 #' Package: \tab koRpus.lang.en\cr
 #' Type: \tab Package\cr
 #' Version: \tab 0.1-4\cr
-#' Date: \tab 2019-05-14\cr
+#' Date: \tab 2020-10-24\cr
 #' Depends: \tab R (>= 3.1),koRpus (>= 0.11-2)\cr
 #' Encoding: \tab UTF-8\cr
 #' License: \tab GPL (>= 3)\cr
